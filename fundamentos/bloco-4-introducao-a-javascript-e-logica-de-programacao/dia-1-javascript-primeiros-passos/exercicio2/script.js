caf5a1ad5d5c1c9e810bd9c32ee73849593d2988
cafe51 <cafecafe51@hotmail.com>
2022-02-03 21:25:47 -0400

    exercicio 2 resolvido

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio2/script.js
@@ -0,0 +1,9 @@
+const base = 5;
+let height = 8; 
+const area = (base*height)
+const perimeter = (2*base + 2*height)
+
+console.log('a base é', base);
+console.log('a altura é', height);
+console.log('a área é', area);
+console.log('o perímetro é', perimeter);
