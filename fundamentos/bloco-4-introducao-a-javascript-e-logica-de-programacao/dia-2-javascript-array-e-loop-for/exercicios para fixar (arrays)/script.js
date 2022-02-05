464408d4dd85bc84b6ceb692eef5574c6c1da8d0
cafe51 <cafecafe51@hotmail.com>
2022-02-05 15:28:39 -0400

    exercicio de fixaçao 1 resolvido

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios para fixar (arrays)/script.js
@@ -0,0 +1,43 @@
+// Exercício 1
+// Obtenha o valor "Serviços" do array menu :
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = 0;
+
+for (index1 = 0; index1 <= menu1.length; index1 += 1){
+    if (menu1[index1] == 'Serviços'){
+        menuServices = menu1[index1]
+    }
+}
+
+console.log(menuServices);
+
+
+// Exercício 2
+// Procure o índice do valor "Portfólio" do array menu :
+
+// let indexOfPortifolio = menu2.indexOf('Portifólio');
+// console.log(indexOfPortifolio);
+
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let contador = []
+
+for (let index2 = 0; menu2[index2] != 'Portfólio'; index2 += 1){
+    contador.push('x')
+}
+
+let indexOfPortfolio = contador.length;
+
+console.log(indexOfPortfolio);
+
+
+// Exercício 3
+// Adicione o valor "Contato" no final do array menu :
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+
+console.log(menu);
+
+
