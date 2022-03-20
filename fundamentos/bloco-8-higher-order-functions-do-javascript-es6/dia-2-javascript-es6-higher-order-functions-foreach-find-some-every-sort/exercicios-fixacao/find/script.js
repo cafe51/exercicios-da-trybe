f0b898a10692961241f3c9e103b9407fb0adde05
cafe51 <cafecafe51@hotmail.com>
2022-03-20 17:54:59 -0400

    exercicio fixacao corrigido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-fixacao/find/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-fixacao/find/script.js
@@ -1,25 +1,25 @@
-// // 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+// 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
 
-// const numbers = [19, 21, 30, 3, 45, 22, 15];
+const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-// const findDivisibleBy3And5 = () => {
-// 	  // Adiciona seu código aqui
-// 	return numbers.find((number) => number % 3 === 0 || number % 5 === 0)
+const findDivisibleBy3And5 = () => {
+	  // Adiciona seu código aqui
+	return numbers.find((number) => number % 3 === 0 || number % 5 === 0)
 
-// }
+}
 
-// console.log(findDivisibleBy3And5())
+console.log(findDivisibleBy3And5())
 
-// // 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
 
-// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
-// const findNameWithFiveLetters = () => {
-// 	return names.find((name) => name.length === 5)
-//   // Adicione seu código aqui:
-// }
+const findNameWithFiveLetters = () => {
+	return names.find((name) => name.length === 5)
+  // Adicione seu código aqui:
+}
 
-// console.log(findNameWithFiveLetters());
+console.log(findNameWithFiveLetters());
 
 // 3 - Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
 
@@ -29,11 +29,9 @@ const musicas = [
   { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
 ]
 
-// const find = (array, key) => array.key
-
 function findMusic(id) {
-	return musicas.find((element) => element.id === id)
   // Adicione seu código aqui
+	return musicas.find((element) => element.id === id)
 }
 
 console.log(findMusic('31031685'))
