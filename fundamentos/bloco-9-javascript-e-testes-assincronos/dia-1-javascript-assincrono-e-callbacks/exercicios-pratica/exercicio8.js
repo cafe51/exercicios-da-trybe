6ea1ddc925ce280d200a23aeab8d71de07c4abd4
cafe51 <cafecafe51@hotmail.com>
2022-04-04 23:12:02 -0400

    exercicio 8 resolvido

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios-pratica/exercicio8.js
@@ -0,0 +1,44 @@
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
+  },
+];
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find((element) => element.name === filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+    const pokemon = pokemons.find((element) => element.name === filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 20);
+}
+
+const mensagem = (erro, result) => {
+	if (erro === null) {
+		console.log(result)
+	} else console.log(erro)
+}
+
+getPokemonDetails('Bulbaasaur', mensagem)
+
+module.exports = {
+  getPokemonDetails,
+};
