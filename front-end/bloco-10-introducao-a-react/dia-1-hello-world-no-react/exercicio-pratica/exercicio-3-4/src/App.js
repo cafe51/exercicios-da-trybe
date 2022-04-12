5eb3f06c3255dbf02112076ca2984844e1f952c7
cafe51 <cafecafe51@hotmail.com>
2022-04-12 03:34:35 -0400

    exercicios 3 e 4 resolvidos

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-pratica/exercicio-3-4/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer';
+
+function App() {
+  return (
+    <body>
+      <header>
+      <Header />
+      </header>
+      <content>
+      <Content />
+    </content>
+    <footer>
+      <Footer />
+    </footer>
+    </body>
+
+  );
+}
+
+export default App;
