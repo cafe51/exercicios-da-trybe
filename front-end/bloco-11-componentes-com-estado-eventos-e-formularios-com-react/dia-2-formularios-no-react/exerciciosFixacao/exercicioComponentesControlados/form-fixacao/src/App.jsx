7b92504174ca168c57fc9e5d0ebd75d7791cb85e
cafe51 <cafecafe51@hotmail.com>
2022-04-23 03:27:59 -0400

    css criado e exercicios 3 a 6 resolvidos

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/exerciciosFixacao/exercicioComponentesControlados/form-fixacao/src/App.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/exerciciosFixacao/exercicioComponentesControlados/form-fixacao/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Form from './Form'
-import './App.css';
+
 
 
 class App extends React.Component {
--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/exerciciosFixacao/exercicioComponentesControlados/form-fixacao/src/Form.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/exerciciosFixacao/exercicioComponentesControlados/form-fixacao/src/Form.jsx
@@ -1,35 +1,96 @@
 import React from 'react'
+import './App.css';
 
 class Form extends React.Component {
   constructor() {
     super()
     this.handleChange = this.handleChange.bind(this)
     this.state = {
-      name: 'saa',
-      email: '23',
+      name: '',
+      email: '',
+      comentario: '',
+      preferencia: '',
+      fixa: 'false',
     }
   }
 
-  handleChange(event) {
+  handleChange({target}) {
+    const { value, name, checked } = target
+    const v = target.type === 'checkbox'? checked : value
     this.setState({
-      name: event.target.value
+      [name]: v,
     })
     // console.log(event.target.value)
   }
 
   render() {
-    const {name, email} = this.state
+    // const {name, email, comentario, preferencia, fixa} = this.state
     return (
-      <form>
-        <label>
-          Nome:
-          <input type="text" value={name} onChange={this.handleChange}></input>
-        </label>
-        <label>
-          Email:
-          <input type="text" value={email}></input>
-        </label>
-      </form>
+      <div className="formbox">
+        <form className="formulario" >
+          <fieldset>
+            <legend>Usuário</legend>
+            <label>
+              Nome:
+              <input 
+              name='name'
+              type="text" 
+              // value={name} 
+              onChange={this.handleChange}
+              ></input>
+            </label>
+
+            <label>
+              Email:
+              <input 
+              name='email' 
+              type="text" 
+              // value={email}
+              onChange={this.handleChange}
+              ></input>
+            </label>
+          </fieldset>
+        
+          <fieldset>
+            <legend>Opções</legend>
+              <label>
+              Comentário:
+              <textarea 
+              name='comentario' 
+              // value={comentario}
+              onChange={this.handleChange}
+              ></textarea>
+            </label>
+
+            <label >
+              Preferência
+              <select  
+              name='preferencia' 
+              // value={preferencia}
+              onChange={this.handleChange}
+              >
+                <option value = 'Azul'> Azul </option>
+                <option value = 'Verde'> Verde</option>
+                <option value = 'Amarelo'> Amarelo</option>
+                <option value='Batata'>Batata </option>
+                <option value = 'Goiania'> Goiania</option>
+              </select>
+            </label>
+
+            <label>
+              Fixa?:
+              <input 
+              name='fixa' 
+              type="checkbox"
+              // value='checked'
+              onChange={this.handleChange}
+              ></input>
+            </label>
+          </fieldset>
+        
+        </form>
+      </div>
+      
     )
   }
 }
